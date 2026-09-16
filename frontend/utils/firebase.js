7a293b14dd04b03af5b8b251d461b3ee6538dcf8
Kartik Sharma <kartiksharna321@gmail.com>
2026-09-16 16:37:30 +0530

    third commit

--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -7,11 +7,11 @@ import { getAuth, GoogleAuthProvider} from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "cortexai-172f9.firebaseapp.com",
-  projectId: "cortexai-172f9",
-  storageBucket: "cortexai-172f9.firebasestorage.app",
-  messagingSenderId: "428104212065",
-  appId: "1:428104212065:web:7948e58a8bb47cda0d1420"
+  authDomain: "rag-based-f623b.firebaseapp.com",
+  projectId: "rag-based-f623b",
+  storageBucket: "rag-based-f623b.firebasestorage.app",
+  messagingSenderId: "210388158886",
+  appId: "1:210388158886:web:c2df94fea97733e5192767"
 };
 
 // Initialize Firebase
