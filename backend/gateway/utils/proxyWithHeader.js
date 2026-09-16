a9be43860dcd2f100d1395f22c63fdc6b8b81d05
Kartik Sharma <kartiksharna321@gmail.com>
2026-09-17 00:49:41 +0530

    Fix gateway proxy body handling

--- backend/gateway/utils/proxyWithHeader.js
+++ backend/gateway/utils/proxyWithHeader.js
@@ -2,9 +2,18 @@ import proxy from "express-http-proxy"
 
 export const proxyWithHeader = (serviceUrl) => {
     return proxy(serviceUrl, {
+        parseReqBody: false,
         reqAsBuffer: true,
         reqBodyEncoding: null,
         limit: "25mb",
+        proxyErrorHandler: (error, res, next) => {
+            console.error("proxy request failed", {
+                serviceUrl,
+                code: error?.code,
+                message: error?.message
+            })
+            next(error)
+        },
         proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
             if (srcReq.user) {
                 proxyReqOpts.headers["x-user-id"] = srcReq.user.userId
